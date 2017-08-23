df3bde00f4411c57ebb1f923dd04b2e4eff5f074
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-08-23 11:48:42 -0500

    El loop de WordPress

--- wp-content/themes/lapizzeria/index.php
+++ wp-content/themes/lapizzeria/index.php
@@ -1,2 +1,14 @@
 <h1>La Pizzería</h1>
 <p>Esta es una plantilla base para WordPress</p>
+<hr />
+
+ <?php
+ /* Loop de WordPress
+    Con este Loop podemos traer todas los "post" publicados al index.
+  */
+  while ( have_posts() ) {
+    the_post();
+    the_title( '<h1>', '</h1>' );
+    the_content();
+  }
+  ?>
