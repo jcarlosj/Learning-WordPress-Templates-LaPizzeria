0458bf1a2e0d933f61467d39eed56118ab99f696
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-08-26 11:21:54 -0500

    Refactoriza la carga de hojas de estilos en WordPress.

--- wp-content/themes/lapizzeria/functions.php
+++ wp-content/themes/lapizzeria/functions.php
@@ -7,13 +7,15 @@
   /* Agregamos la función que nos permitirá hacer uso de una hoja de estilos
      para la plantilla */
   function lapizzeria_styles() {
-    /**/
-    wp_enqueue_style(
-      'style',                                      # Nombre que toma la función wp_enqueue_style
+    /* Registra las funciones en el Core de Wordpress */
+    wp_register_style(
+      'style',                                      # Nombre que toma la función registrada en el Core de Wordpress
       get_template_directory_uri() . '/style.css',  # Ruta del fichero en el directorio CSS de la plantilla
       array(),                                      # Dependencias (ficheros que deseamos que se carguen antes, vacio por ahora)
       '1.0'                                         # Versión de la hoja de estilos
     );
+    /* Agregamos el  estilo registrado */
+    wp_enqueue_style( 'style' );
   }
 
   /* Agrega las acciones creadas al Core de WordPress:
