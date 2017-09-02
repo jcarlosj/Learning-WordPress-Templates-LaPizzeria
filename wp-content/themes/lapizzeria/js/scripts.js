bbb2866fdbd79150649a1455a94060d2d91e93b9
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-09-02 13:43:35 -0500

    Agrega funcionalidad al menú RWD para mostrarse u ocultarse por interacción y por redimensión de la ventana del navegador

--- wp-content/themes/lapizzeria/js/scripts.js
+++ wp-content/themes/lapizzeria/js/scripts.js
@@ -4,5 +4,20 @@
 $ = jQuery.noConflict();    /* Elimina Error de Referencia cuando el $ no aparece definido */
 
 $( document ) .ready( function() {
-    alert( 'Cargando los archivos JS con éxito, con jQuery' );
-});
+    /* Agrega funcionalidad al botón para mostrar u ocultar el menú */
+    $( '.button-mobile-menu a' ) .on( 'click', function() {
+      $( 'nav.site-menu' ) .toggle( 'slow' );
+    });   // ( '.button-mobile-menu a' )
+
+    /* Muestra u oculta el menú al redimensionar la ventana del navegador */
+    var breakpoint = 768;
+    $( window ) .resize( function() {
+      /* console .log( $( document ) .width() + ' px'); */
+      if( $( document ) .width() >= breakpoint ) {
+        $( 'nav.site-menu' ) .show();
+      }
+      else {
+        $( 'nav.site-menu' ) .hide();
+      }
+    });   // $( window )
+});   // $( document )
