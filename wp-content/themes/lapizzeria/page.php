f94f93fe5c34c2ffaaed36979b8adae1b3144514
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-09-04 12:21:50 -0500

    Agrega estilos para la página de nosotros

--- wp-content/themes/lapizzeria/page.php
+++ wp-content/themes/lapizzeria/page.php
@@ -1,10 +1,15 @@
 <?php get_header(); ?>
 
 <?php while( have_posts() ): the_post(); # Agregamos el loop de WordPress ?>
-  <?php the_post_thumbnail(); # Template Tag: para imprimir imagen destadaca de la página ?>
-  <h1><?php the_title();      # Template Tag: para imprimir el título de la página ?></h1>
+  <div class="hero" style="background-image: url( <?php echo get_the_post_thumbnail_url(); ?> );">
+    <div class="hero-content">
+      <div class="hero-text">
+        <h1><?php the_title();      # Template Tag: para imprimir el título de la página ?></h1>
+      </div>
+    </div>
+  </div>
   <div class="principal content">
-    <main>
+    <main class="centered-text page-content">
       <?php the_content();    # Template Tag: para imprimir el contenido de la página ?>
     </main>
   </div>
