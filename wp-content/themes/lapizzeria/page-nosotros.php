8df175a1ed50355c0466c5dff07bc234321fd552
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-09-04 14:51:13 -0500

    Personalizamos el contenido de la página de nosotros. Instalamos el plugin 'Advanced Custom Fields' y registramos los campos necesarios para el despliegue del nuevo contenido en esta vista

--- wp-content/themes/lapizzeria/page-nosotros.php
+++ wp-content/themes/lapizzeria/page-nosotros.php
@@ -1,3 +1,21 @@
-<h1>Nosotros</h1>
-<p>Personalización de la vista de Nosotros usando un <b>Default Template</b> de la forma <i>page-<b>$slug</b>.php</i> teniendo encuenta la gerarquía de temas de WordPress.</p>
-<p>Esta vista hace uso del archivo <i>page-<b>nosotros</b>.php</i> contenido en nuestra plantilla "lapizzeria"</p>
+<?php get_header(); ?>
+
+<?php while( have_posts() ): the_post(); # Agregamos el loop de WordPress ?>
+  <div class="hero" style="background-image: url( <?php echo get_the_post_thumbnail_url(); ?> );">
+    <div class="hero-content">
+      <div class="hero-text">
+        <h1><?php the_title();      # Template Tag: para imprimir el título de la página ?></h1>
+      </div>
+    </div>
+  </div>
+  <div class="principal content">
+    <main class="centered-text page-content">
+      <?php the_content();    # Template Tag: para imprimir el contenido de la página ?>
+    </main>
+  </div>
+
+  <h2>Detalles</h2>
+  <p>Agregaremos más detalles a la vista de nosotros. Para ello instalaremos el plugin <b>Advanced Custom Fields</b> y registraremos los campos que estarán disponibles en el ADMIN de WordPress para generar contenido en este espacio de la vista personalizada de nosotros <i>"page-<b>nosotros</b>.php"</i>. </p>
+<?php endwhile; ?>
+
+<?php get_footer(); ?>
