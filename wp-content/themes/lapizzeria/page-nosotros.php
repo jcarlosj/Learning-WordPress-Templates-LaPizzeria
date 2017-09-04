1a43bdb1aaf6aa9fb3d32e817db872eb1b8a4af9
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-09-04 14:12:27 -0500

    Crea archivo de vista para la personalización de la pagina de nosotros

---
+++ wp-content/themes/lapizzeria/page-nosotros.php
@@ -0,0 +1,3 @@
+<h1>Nosotros</h1>
+<p>Personalización de la vista de Nosotros usando un <b>Default Template</b> de la forma <i>page-<b>$slug</b>.php</i> teniendo encuenta la gerarquía de temas de WordPress.</p>
+<p>Esta vista hace uso del archivo <i>page-<b>nosotros</b>.php</i> contenido en nuestra plantilla "lapizzeria"</p>
