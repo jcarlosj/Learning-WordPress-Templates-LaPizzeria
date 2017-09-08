1436a8b1418dc7cb2dc663c305e552c71c136e64
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-09-08 13:44:53 -0500

    Agrega formulario de comentarios a un 'single.php'

--- wp-content/themes/lapizzeria/single.php
+++ wp-content/themes/lapizzeria/single.php
@@ -1,5 +1,22 @@
 <?php get_header(); ?>
-    <h1>La Pizzería</h1>
-    <h3>single.php</h3>
-    <p>Esta es una plantilla base para WordPress desde <b>single.php</b></p>
+
+<?php while( have_posts() ): the_post(); # Agregamos el loop de WordPress ?>
+  <div class="hero" style="background-image: url( <?php echo get_the_post_thumbnail_url(); ?> );">
+    <div class="hero-content">
+      <div class="hero-text">
+        <h1><?php the_title();      # Template Tag: para imprimir el título de la página ?></h1>
+      </div>
+    </div>
+  </div>
+  <div class="principal content">
+    <main class="centered-text page-content">
+      <?php the_content();    # Template Tag: para imprimir el contenido de la página ?>
+    </main>
+  </div>
+<?php endwhile; ?>
+
+<div class="comments content">
+  <?php comment_form(); ?>
+</div>
+
 <?php get_footer(); ?>
