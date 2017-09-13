ed6f02295e8ac334df4cf11c336432c609d8a998
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-09-13 11:58:33 -0500

    Inserta los datos del formulario de reserva en la ṕagina de Contacto y crea una redirección a una página previamente creada desde el ADMIN

--- wp-content/themes/lapizzeria/includes/reservaciones.php
+++ wp-content/themes/lapizzeria/includes/reservaciones.php
@@ -5,21 +5,51 @@
 
   /* Evento Guardar */
   function lapizzeria_save() {
-    if( isset( $_POST[ 'enviar' ] ) && $_POST[ 'save' ] == 1 ) {
-      /* Recogemos los datos del formulario */
-      echo '<code>Formulario <pre>'; var_dump( $_POST ); echo '</pre></code>';
 
-      /* Sanitizando datos del formulario (Cuando entran los datos) */
+    global $wpdb;   # Clase de acceso a las funciones de CRUD de WordPress
+
+    if( isset( $_POST[ 'enviar' ] ) && $_POST[ 'save' ] == 1 ) {
+
+      $name_table      = $wpdb -> prefix .'reservaciones'; # Prefijo de la tabla fijado en la configuración inicial
+
+      # To Debug
+      # echo '<code>Formulario <pre>'; var_dump( $_POST ); echo '</pre></code>';
+
+      /* Recogemos los datos del formulario
+         Sanitizando los datos del formulario (Cuando entran los datos) */
       $reservation_data = array(
         'nombre'   => lapizzeria_sanitize_text_field( 'nombre' ),
         'fecha'    => lapizzeria_sanitize_text_field( 'fecha' ),
-        'email'    => lapizzeria_sanitize_text_field( 'email' ),
+        'correo'   => lapizzeria_sanitize_text_field( 'email' ),
         'telefono' => lapizzeria_sanitize_text_field( 'telefono' ),
         'mensaje'  => lapizzeria_sanitize_text_field( 'mensaje' )
       );
 
-      /* Recogemos los datos del formulario */
-      echo '<code>Sanatizado <pre>'; var_dump( $reservation_data ); echo '</pre></code>';
+      # To Debug
+      # echo '<code>Sanatizado <pre>'; var_dump( $reservation_data ); echo '</pre></code>';
+
+      /* Formato de tipos para la inserción de datos */
+      $data_types = array(
+        '%s',     # Tipo 'String' para nombre
+        '%s',     # Tipo 'String' para fecha
+        '%s',     # Tipo 'String' para email
+        '%s',     # Tipo 'String' para telefono
+        '%s'      # Tipo 'String' para mensaje
+      );
+
+      /* Realizamos inserción de los datos a la base de datos, usando el Objeto $wpdb */
+      $done = $wpdb -> insert(
+        $name_table,        # Nombre de la tabla en la base de datos
+        $reservation_data,  # Nombre del Array que contiene los datos Sanitizados
+        $data_types         # Nombre del Array con los tipos de datos que vamos a insertar en la tabla
+      );
+      # To Debug
+      # echo '<code>Inserta <pre>'; var_dump( $done ); echo '</pre></code>';
+
+      /* Realizamos la redirección (Así evitamos además que haga multiples registros al actualizar la página) */
+      $url = get_page_by_title( 'Gracias por su reserva' );   # Título de la página de agradecimiento creada desde el ADMIN
+      wp_redirect( get_permalink( $url -> ID ) );
+      exit();
 
     }
   }
@@ -29,5 +59,5 @@
     return sanitize_text_field( $_POST[ $field_name ] );
   }
 
-  add_action( 'init', 'lapizzeria_save' );    # Hook al init
+  add_action( 'init', 'lapizzeria_save' );    # Hook al init (Se ejecuta todo el tiempo en el que WordPress este ejecutandose)
 ?>
