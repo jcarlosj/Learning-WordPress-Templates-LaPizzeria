ca2747f63acc13ddc23d3f83c45fbec16492fb92
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-09-13 11:09:59 -0500

    Sanitiza los datos del formulario de reserva que provienen de la página de Contacto

--- wp-content/themes/lapizzeria/includes/reservaciones.php
+++ wp-content/themes/lapizzeria/includes/reservaciones.php
@@ -7,9 +7,26 @@
   function lapizzeria_save() {
     if( isset( $_POST[ 'enviar' ] ) && $_POST[ 'save' ] == 1 ) {
       /* Recogemos los datos del formulario */
-      echo '<code><pre>'; var_dump( $_POST ); echo '</pre></code>';
-      
+      echo '<code>Formulario <pre>'; var_dump( $_POST ); echo '</pre></code>';
+
+      /* Sanitizando datos del formulario (Cuando entran los datos) */
+      $reservation_data = array(
+        'nombre'   => lapizzeria_sanitize_text_field( 'nombre' ),
+        'fecha'    => lapizzeria_sanitize_text_field( 'fecha' ),
+        'email'    => lapizzeria_sanitize_text_field( 'email' ),
+        'telefono' => lapizzeria_sanitize_text_field( 'telefono' ),
+        'mensaje'  => lapizzeria_sanitize_text_field( 'mensaje' )
+      );
+
+      /* Recogemos los datos del formulario */
+      echo '<code>Sanatizado <pre>'; var_dump( $reservation_data ); echo '</pre></code>';
+
     }
+  }
+
+  /* Aplica la Sanitización de tipo 'sanitize_text_field' de WordPress */
+  function lapizzeria_sanitize_text_field( $field_name ) {
+    return sanitize_text_field( $_POST[ $field_name ] );
   }
 
   add_action( 'init', 'lapizzeria_save' );    # Hook al init
