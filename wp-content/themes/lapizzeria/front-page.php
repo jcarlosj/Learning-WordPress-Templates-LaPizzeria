ce864eaa63c022101af4745abf8027ecf8fbff17
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-09-17 14:43:05 -0500

    Imprim[e Título y Slogan en la página principal (El primero obtenido de la descripción de los ajustes generales de la página, el segundo del contenido de la página de inicio en el ADMIN de WordPress)

--- wp-content/themes/lapizzeria/front-page.php
+++ wp-content/themes/lapizzeria/front-page.php
@@ -4,13 +4,24 @@
   <div class="hero" style="background-image: url( <?php echo get_the_post_thumbnail_url(); ?> );">
     <div class="hero-content">
       <div class="hero-text">
-        <h1>Front Page<?php # the_title();      # Template Tag: para imprimir el título de la página ?></h1>
+        <h1>
+          <?php
+              bloginfo( 'description' );                          # O puedes usar una segunda opción
+              #echo esc_html( get_option( 'blogdescription' ) );  # que traerá en ambos casos la 'descripción corta'
+                                                                  # de la sección 'Ajuestes generales' en el ADMIN de WordPress
+            ?>
+        </h1>
+        <?php
+          the_content();
+          $url = get_page_by_title( 'Nosotros' );
+        ?>
+        <a class="button" href="<?php echo get_permalink( $url -> ID ); ?>">Leer más</a>
       </div>
     </div>
   </div>
   <div class="principal content">
     <main class="centered-text page-content">
-      Hola desde <b>front-page.php</b>
+      Hola desde el <b>front-page.php</b>
       <?php #the_content();    # Template Tag: para imprimir el contenido de la página ?>
     </main>
   </div>
