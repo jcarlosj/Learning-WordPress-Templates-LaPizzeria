2488a62a13d3644f222a39aec7901f327d519b09
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-01-12 17:00:55 -0500

    Crea ventana modal para confirmar si se desea eliminar el registro usando SweetAlert 2

--- wp-content/themes/lapizzeria/js/admin-ajax.js
+++ wp-content/themes/lapizzeria/js/admin-ajax.js
@@ -13,34 +13,50 @@ $( document ) .ready( function() {
     var id = $( this ) .attr( 'data-reservations' );  /* Obtenemos el ID del elemento en el atributo especificado */
     // console .log( id );
 
-    /* Crea petición AJAX */
-    $.ajax({
-      type: 'post',     /* Tipo de petición */
-      data: {           /* El conjunto de datos que se desean enviar */
-          'action' : 'lapizzeria_delete',     /* nombre de la función a ejecutar la acción */
-          'id'     : id,                      /* ID del registro que vamos a eliminar */
-          'tipo_registro' : 'eliminar'
-      },
-      url: delete_reservation .ajax_url,      /* URL del archivo PHP que va a ejecutar la acción (que se va a encargar de esta petición AJAX) */
-      success : function( data ) {            /* Se ejecutará cuando la petición al archivo de AJAX sea correcta */
-        console .log( data );                 /* Datos que recibimos como respuesta en un objeto JSON */
+    swal({
+      title: '¿Esta seguro?',
+      text: "Esta acción no se puede deshacer!",
+      type: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Sí, eliminalo!',
+      cancelButtonText: 'Cancelar'
+    })
+    .then( ( result ) => {
 
-        var result = JSON .parse( data );     /* Convierte el String JSON a un Objeto JavaScript */
+      if ( result .value ) {
+        /* Crea petición AJAX */
+        $.ajax({
+          type: 'post',     /* Tipo de petición */
+          data: {           /* El conjunto de datos que se desean enviar */
+              'action' : 'lapizzeria_delete',     /* nombre de la función a ejecutar la acción */
+              'id'     : id,                      /* ID del registro que vamos a eliminar */
+              'tipo_registro' : 'eliminar'
+          },
+          url: delete_reservation .ajax_url,      /* URL del archivo PHP que va a ejecutar la acción (que se va a encargar de esta petición AJAX) */
+          success : function( data ) {            /* Se ejecutará cuando la petición al archivo de AJAX sea correcta */
+            console .log( data );                 /* Datos que recibimos como respuesta en un objeto JSON */
 
-        if( result .respuesta == 1 ) {
-          /* Buscamos el objeto del DOM que deseamos remover */
-          $( '[data-reservations="' + result .id + '"]' ) .parent() .parent() .remove();
+            var resultado = JSON .parse( data );     /* Convierte el String JSON a un Objeto JavaScript */
 
-          /* Despliega mensaje emergente SweetAlert 2 */
-          swal(
-            'Reserva Eliminada',
-            'Se eliminó el registro correctamente!',
-            'success'       /* Define el tipo de Alerta en SweetAlert 2 */
-          );
-        }
+            if( resultado .respuesta == 1 ) {
+              /* Buscamos el objeto del DOM que deseamos remover */
+              $( '[data-reservations="' + resultado .id + '"]' ) .parent() .parent() .remove();
 
-      }
-    });
+              /* Despliega mensaje emergente SweetAlert 2 */
+              swal(
+                'Reserva Eliminada',
+                'Se eliminó el registro correctamente!',
+                'success'       /* Define el tipo de Alerta en SweetAlert 2 */
+              );
+            }
+
+          }
+        }); /* $.ajax */
+      }   /* if ( result .value ) */
+
+    }); /* swal/then */
 
   });
 
