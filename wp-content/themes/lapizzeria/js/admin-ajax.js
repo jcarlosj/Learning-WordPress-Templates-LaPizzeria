26f89832fc3058d1e15adfadc67c38f5201b727e
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-01-12 11:24:47 -0500

    Obtiene el ID del registro usando evento 'click' de jQuery

--- wp-content/themes/lapizzeria/js/admin-ajax.js
+++ wp-content/themes/lapizzeria/js/admin-ajax.js
@@ -4,4 +4,13 @@ $ = jQuery .noConflict();
 $( document ) .ready( function() {
   /* Obtener la URL de admin-ajax.php */
   console .log( delete_reservation .ajax_url );     /* 'delete_reservation' retorna el objeto JS generado en el 'function.php' */
+
+  $( '.delete-register' ) .on( 'click', function( event ) {
+    event .preventDefault();        /* preventDefault() es una función de JavaScript previene el evento por defecto que se registre en un formulario, enlace o cualquier elemento del HTML */
+
+    /* this hace referencia al elemento que lo ejecuta */
+    var id = $( this ) .attr( 'data-reservations' );  /* Obtenemos el ID del elemento en el atributo especificado */
+    console .log( id );
+  });
+
 });
